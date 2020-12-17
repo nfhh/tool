faacd973ccc36e7e67a184e3745d0cb0d68690e5
Stoa <542950749@qq.com>
2020-12-17 11:34:14 +0800

    后台用户权限判断

--- resources/views/layouts/_left.blade.php
+++ resources/views/layouts/_left.blade.php
@@ -1,8 +1,10 @@
 <div class="list-group" id="a_box">
+    @if(auth()->user()->role==1)
     <a href="/user" class="list-group-item list-group-item-action">用户列表</a>
     <a href="/sn" class="list-group-item list-group-item-action">生成SN</a>
     <a href="/test" class="list-group-item list-group-item-action">测试结果列表</a>
     <a href="/product" class="list-group-item list-group-item-action">型号管理</a>
+    @endif
     <a href="/weight" class="list-group-item list-group-item-action">SN与重量绑定</a>
 </div>
 <script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        退出
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
