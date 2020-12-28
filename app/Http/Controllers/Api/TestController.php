bd3ab0745c201b702644b11fa777e1d9d35829df
Stoa <542950749@qq.com>
2020-12-28 16:06:34 +0800

    sn

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Test;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
@@ -10,9 +11,16 @@ class TestController extends Controller
     public function store(Request $request)
     {
         $data = $request->all();
+        $sn = $data['sn'];
+        if(Test::where('sn',$sn)->first()){
+            return response()->json([
+                'code' => -1,
+                'message' => 'sn重复！',
+            ]);
+        }
         $z_data = $data['postData']['init'];
         $z_data['shell_res'] = $data['postData']['shell_res'];
-        $z_data['sn'] = $data['sn'];
+        $z_data['sn'] = $sn;
         auth('api')->user()->tests()->create($z_data);
         return response()->json([
             'code' => 0,
